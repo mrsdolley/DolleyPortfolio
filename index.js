b47a83367fff8d60ebc90cebd48ce323751ed896
mrsdolley <40613728+mrsdolley@users.noreply.github.com>
2018-06-28 11:25:09 -0400

    DolleyJavaScriptExercises

---
+++ index.js
@@ -0,0 +1,28 @@
+//Exercise One
+var command = "Enter a number!";
+var number = prompt(command);
+
+var output = 0;
+
+for (var num = 0; num <= number; num++) 
+{output = output + num; 
+}
+
+alert(output);
+
+//Excerise Two
+var questionOne = "Do you want to play?";
+prompt(questionOne);
+
+var firstAnswer = "yes";
+
+var command = "Enter a word";
+
+// var questionTwo = "Do you want to play again?";
+
+// var secondAnswer = "no";
+// //var finalAnswer = 
+
+// if(questionOne = firstAnswer) {prompt(command);} else { alert(secondAnswer);}
+
+
